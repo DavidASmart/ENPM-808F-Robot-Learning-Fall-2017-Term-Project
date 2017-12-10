3e77b1bd1f533cd32f1b7c16feb0f4bf68db4c88
DavidASmart <34402541+DavidASmart@users.noreply.github.com>
2017-12-10 11:58:01 -0500

    Update Extended_AutoCar_QLearning.m

--- Extended_AutoCar_QLearning.m
+++ Extended_AutoCar_QLearning.m
@@ -44,7 +44,7 @@ numrc = 10; % Size of world
 load('Q_b.mat')
                     
 % extend the number of training episoides
-numepisodes_old = numepisodes-episode;
+numepisodes_old = numepisodes;
 numepisodes = numepisodes_old + ((numrc*2)*(numrc*2)*(2^4)*(5)*(5));
 
 
@@ -76,11 +76,11 @@ for episode = numepisodes_old:numepisodes
     
     % epsilon = 0.75 + (0.01 - 0.75)*(episode/numepisodes);
     % reduced epsilon...but not just continuing lower
-    epsilon = 0.375 + (0.005 - 0.375)*(episode-numepisodes_old)/(numepisodes-numepisodes_old);
+    epsilon = 0.375 + (0 - 0.375)*(episode-numepisodes_old)/(numepisodes-numepisodes_old);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % periodic episode-gif creation & Q-matrix backups
-    if (mod(episode,numepisodes/50) == 1)
+    if (mod(episode-numepisodes_old,(numepisodes-numepisodes_old)/50) == 1)
 %         make_animated_gif('clear')
         save('Q_b_extended.mat');
 %     elseif (mod(episode,numepisodes/50) == 2)
@@ -102,75 +102,9 @@ for episode = numepisodes_old:numepisodes
         AGENTcontroler
                 
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % State Update
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
-        % recalculate distance to goal
-        dgx_next = agent.x - goalx;
-        dgy_next = agent.y - goaly;
-        % recalculate is there an iminent crash ?
-        ca_next = CrashEval_agent(agent,Walls,nWalls,Cars,nCars);
-        % convert to index-value
-        cIndex_next = ca_next*[2^0;2^1;2^2;2^3]+1;
-        
-        % check if crashed into walls
-        for n = 1:nWalls
-            if ((agent.x - Walls(n).x)^2 +(agent.y - Walls(n).y)^2 == 0)
-                crashed = 1;
-                if (a ~= 5)
-                    if (ca(a) == 0) % check if CrashEval_agent works perfectly
-                        EvalErr = EvalErr+1; 
-                    end
-                end
-            end
-        end
-        
-        % check if crashed into cars
-        for n = 1:nCars
-            if ((agent.x - Cars(n).x)^2 +(agent.y - Cars(n).y)^2 == 0)
-                crashed = 1;
-                if (a ~= 5)
-                    if (ca(a) == 0) % check if CrashEval_agent works perfectly
-                        EvalErr = EvalErr+1; 
-                    end
-                end
-            end
-        end
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Q-learning
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % evaluate reward
-        if (crashed == 1)
-            r = -2;
-        elseif (dgx_next^2 + dgy_next^2 == 0)
-            r = 2;
-            goalreached = 1;
-        else
-            r = 0; % -0.01;
-        end
-        
-        % update alpha
-        saCounter(dgx+numrc,dgy+numrc,cIndex,ocd_old, a) = saCounter(dgx+numrc,dgy+numrc,cIndex,ocd_old, a) + 1;
-        alpha = 1 / saCounter(dgx+numrc,dgy+numrc,cIndex,ocd_old,a);
-        
-        % update Q-value
-        Q_current = Q(dgx+numrc, dgy+numrc, cIndex, ocd_old, a);
-        Q_next = max(Q(dgx_next+numrc, dgy_next+numrc, cIndex_next, ocd, :));
-        Q(dgx+numrc, dgy+numrc, cIndex, ocd_old, a) = (1-alpha)*Q_current + alpha*(r + gamma*Q_next);
-        % so that we don't calculate the same thing twice...
-        dgx = dgx_next;
-        dgy = dgy_next;
-        ca = ca_next;
-        cIndex = cIndex_next;
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % update time
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         t = t + dt;
-        if (t >= maxt)
-            timeout = 1;
-        end
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % update world display
@@ -188,6 +122,78 @@ for episode = numepisodes_old:numepisodes
 %         if (mod(episode,numepisodes/50) == 1)
 %             make_animated_gif('snap')
 %         end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % State Update
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % recalculate distance to goal
+        dgx_next = agent.x - goalx;
+        dgy_next = agent.y - goaly;
+        % recalculate is there an iminent crash ?
+        ca_next = CrashEval_agent(agent,Walls,nWalls,Cars,nCars);
+        % convert to index-value
+        cIndex_next = ca_next*[2^0;2^1;2^2;2^3]+1;
+        
+       % check if crashed into walls
+        for n = 1:nWalls
+            if ((agent.x - Walls(n).x)^2 +(agent.y - Walls(n).y)^2 < 1)
+                crashed = 1;
+                numcrashes = numcrashes+1;
+                if (a ~= 5)
+                    if (ca(a) == 0) % check if CrashEval_agent works perfectly
+                        EvalErr = EvalErr+1; 
+                    end
+                end
+            end
+        end
+        % check if crashed into cars
+        for n = 1:nCars
+            if ((agent.x - Cars(n).x)^2 +(agent.y - Cars(n).y)^2 < 1)
+                crashed = 1;
+                numcrashes = numcrashes+1;
+                if (a ~= 5)
+                    if (ca(a) == 0) % check if CrashEval_agent works perfectly
+                        EvalErr = EvalErr+1; 
+                    end
+                end
+            end
+        end
+        % check if reached goal
+        if (crashed == 0)
+            if (dgx^2 + dgy^2 == 0)
+                goalreached = 1;
+                goalsreached = goalsreached+1;
+            elseif (t >= maxt)
+                timeout = 1;
+                timeouts = timeouts+1;
+            end
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Q-learning
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % evaluate reward
+        if (crashed == 1)
+            r = -2;
+        elseif (goalreached == 1)
+            r = 2;
+        else
+            r = 0; % -0.01;
+        end
+        
+        % update alpha
+        saCounter(dgx+numrc,dgy+numrc,cIndex,ocd_old, a) = saCounter(dgx+numrc,dgy+numrc,cIndex,ocd_old, a) + 1;
+        alpha = 1 / saCounter(dgx+numrc,dgy+numrc,cIndex,ocd_old,a);
+        
+        % update Q-value
+        Q_current = Q(dgx+numrc, dgy+numrc, cIndex, ocd_old, a);
+        Q_next = max(Q(dgx_next+numrc, dgy_next+numrc, cIndex_next, ocd, :));
+        Q(dgx+numrc, dgy+numrc, cIndex, ocd_old, a) = (1-alpha)*Q_current + alpha*(r + gamma*Q_next);
+        % so that we don't calculate the same thing twice...
+        dgx = dgx_next;
+        dgy = dgy_next;
+        ca = ca_next;
+        cIndex = cIndex_next;
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % keep track of stats
@@ -274,6 +280,7 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % save trained Q-matrix
+save('Q_b_extended.mat');
 save('Q_extended.mat','Q');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
